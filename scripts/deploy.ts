1a7a8784796c0654a2585fa6eb87166ff5034061
Edison Lee <edisonlee@edisonlee55.com>
2023-12-07 15:23:33 +0800

    chore: update deploy scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,4 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
 import dotenv from 'dotenv';
 dotenv.config();
 
--- scripts/deploy_safe.ts
+++ scripts/deploy_safe.ts
@@ -1,5 +1,6 @@
 import { ethers } from 'hardhat';
 import { EthersAdapter, ContractNetworksConfig, SafeFactory, SafeAccountConfig, DEFAULT_SAFE_VERSION } from '@safe-global/protocol-kit';
+import { randomBytes } from 'crypto';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -14,25 +15,29 @@ async function main() {
     const owner1Address = await ethAdapterOwner1.getSignerAddress();
 
     const safeVersion = process.env.SAFE_VERSION || DEFAULT_SAFE_VERSION;
-    const chainId = await ethAdapterOwner1.getChainId();
-    const contractNetworks: ContractNetworksConfig = {
-        [chainId]: {
-            safeSingletonAddress: process.env.SAFE_SINGLETON_ADDRESS!,
-            safeProxyFactoryAddress: process.env.SAFE_PROXY_FACTORY_ADDRESS!,
-            multiSendAddress: process.env.MULTI_SEND_ADDRESS!,
-            multiSendCallOnlyAddress: process.env.MULTI_SEND_CALL_ONLY_ADDRESS!,
-            fallbackHandlerAddress: process.env.FALLBACK_HANDLER_ADDRESS!,
-            signMessageLibAddress: process.env.SIGN_MESSAGE_LIB_ADDRESS!,
-            createCallAddress: process.env.CREATE_CALL_ADDRESS!,
-            simulateTxAccessorAddress: process.env.SIMULATE_TX_ACCESSOR_ADDRESS!
-        }
-    };
-
-    console.log(`Deploying to network: ${network.name} (${network.chainId})`);
+    console.log(`Deploying Safe ${safeVersion} to network: ${network.name} (${network.chainId})`);
     console.log(`Owner 1 address: ${owner1Address}`);
 
-    // Only support networks that are added to https://github.com/safe-global/safe-deployments
-    const safeFactory = await SafeFactory.create({ ethAdapter: ethAdapterOwner1, contractNetworks, safeVersion });
+    let safeFactory;
+    if (process.env.USE_SAFE_DEPLOYMENTS === "false") {
+        const contractNetworks: ContractNetworksConfig = {
+            [network.chainId]: {
+                safeSingletonAddress: process.env.SAFE_SINGLETON_ADDRESS,
+                safeProxyFactoryAddress: process.env.SAFE_PROXY_FACTORY_ADDRESS,
+                multiSendAddress: process.env.MULTI_SEND_ADDRESS,
+                multiSendCallOnlyAddress: process.env.MULTI_SEND_CALL_ONLY_ADDRESS,
+                fallbackHandlerAddress: process.env.FALLBACK_HANDLER_ADDRESS,
+                signMessageLibAddress: process.env.SIGN_MESSAGE_LIB_ADDRESS,
+                createCallAddress: process.env.CREATE_CALL_ADDRESS,
+                simulateTxAccessorAddress: process.env.SIMULATE_TX_ACCESSOR_ADDRESS
+            }
+        };
+
+        safeFactory = await SafeFactory.create({ ethAdapter: ethAdapterOwner1, contractNetworks, safeVersion });
+    } else {
+        // Only support networks that are added to https://github.com/safe-global/safe-deployments
+        safeFactory = await SafeFactory.create({ ethAdapter: ethAdapterOwner1, safeVersion });
+    }
 
     const safeAccountConfig: SafeAccountConfig = {
         owners: [
@@ -41,9 +46,16 @@ async function main() {
         threshold: 1
     };
 
-    const safeSdkOwner1 = await safeFactory.deploySafe({ safeAccountConfig });
-    const safeAddress = await safeSdkOwner1.getAddress();
+    let safeSdkOwner1;
+    if (process.env.USE_RANDOM_SALT_NONCE === "false") {
+        safeSdkOwner1 = await safeFactory.deploySafe({ safeAccountConfig });
+    } else {
+        const saltNonce = `0x${randomBytes(32).toString('hex')}`;
+        console.log(`Using random saltNonce for Safe deployment: ${saltNonce}`);
+        safeSdkOwner1 = await safeFactory.deploySafe({ safeAccountConfig, saltNonce });
+    }
 
+    const safeAddress = await safeSdkOwner1.getAddress();
     console.log('Your Safe has been deployed:');
     console.log(`${safeAddress}`);
 };
