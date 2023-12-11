53cbb6f469589e6bb913f05da64da9c799fdd3a0
Edison Lee <edisonlee@edisonlee55.com>
2023-12-11 17:09:14 +0800

    build: support Safe L2 Singleton

--- scripts/deploy_safe.ts
+++ scripts/deploy_safe.ts
@@ -1,5 +1,6 @@
 import { ethers } from 'hardhat';
 import { EthersAdapter, ContractNetworksConfig, SafeFactory, SafeAccountConfig, DEFAULT_SAFE_VERSION } from '@safe-global/protocol-kit';
+import { SafeVersion } from '@safe-global/safe-core-sdk-types';
 import { randomBytes } from 'crypto';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -18,11 +19,17 @@ async function main() {
     console.log(`Deploying Safe ${safeVersion} to network: ${network.name} (${network.chainId})`);
     console.log(`Owner 1 address: ${owner1Address}`);
 
+    if (process.env.USE_L1_SAFE_SINGLETON === "true") {
+        console.log("Using L1 Safe Singleton");
+    } else {
+        console.log("Using L2 Safe Singleton");
+    }
+
     let safeFactory;
     if (process.env.USE_SAFE_DEPLOYMENTS === "false") {
         const contractNetworks: ContractNetworksConfig = {
             [network.chainId]: {
-                safeSingletonAddress: process.env.SAFE_SINGLETON_ADDRESS || "0x41675C099F32341bf84BFc5382aF534df5C7461a",
+                safeSingletonAddress: process.env.USE_L1_SAFE_SINGLETON === "true" ? process.env.SAFE_SINGLETON_ADDRESS || "0x41675C099F32341bf84BFc5382aF534df5C7461a" : process.env.SAFE_L2_SINGLETON_ADDRESS || "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
                 safeProxyFactoryAddress: process.env.SAFE_PROXY_FACTORY_ADDRESS || "0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67",
                 multiSendAddress: process.env.MULTI_SEND_ADDRESS || "0x38869bf66a61cF6bDB996A6aE40D5853Fd43B526",
                 multiSendCallOnlyAddress: process.env.MULTI_SEND_CALL_ONLY_ADDRESS || "0x9641d764fc13c8B624c04430C7356C1C7C8102e2",
@@ -33,10 +40,10 @@ async function main() {
             }
         };
 
-        safeFactory = await SafeFactory.create({ ethAdapter: ethAdapterOwner1, contractNetworks, safeVersion });
+        safeFactory = await SafeFactory.create({ ethAdapter: ethAdapterOwner1, contractNetworks, safeVersion: safeVersion as SafeVersion, isL1SafeSingleton: Boolean(process.env.USE_L1_SAFE_SINGLETON) });
     } else {
         // Only support networks that are added to https://github.com/safe-global/safe-deployments
-        safeFactory = await SafeFactory.create({ ethAdapter: ethAdapterOwner1, safeVersion });
+        safeFactory = await SafeFactory.create({ ethAdapter: ethAdapterOwner1, safeVersion: safeVersion as SafeVersion, isL1SafeSingleton: Boolean(process.env.USE_L1_SAFE_SINGLETON) });
     }
 
     const safeAccountConfig: SafeAccountConfig = {
